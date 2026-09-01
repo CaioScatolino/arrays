7c31c6e7fa7f84470f4039c4cc96282886d7eacd
Caio <caio.scatolino@outlook.com>
2026-09-01 19:12:46 -0300

    add initial implementation of array filtering and mapping

---
+++ treiando_arrays.js
@@ -0,0 +1,27 @@
+let lista = ['maçã', 'banana', 'laranja', 'uva', 'abacaxi']
+
+
+let lista2 = lista.filter((item) => {
+    return item.length > 5
+})
+
+console.log(lista2)
+
+
+let precos = [
+    { "item": "maçã", "preco": 2.5 },
+    { "item": "banana", "preco": 1.5 },
+    { "item": "laranja", "preco": 3.0 },
+    { "item": "uva", "preco": 4.0 },
+    { "item": "abacaxi", "preco": 5.0 }
+]
+
+let precosFiltrados = precos.filter((produto) => {
+    return produto.preco > 3.0
+})
+
+let produtos = precosFiltrados.map((produto) => {
+    return produto.item
+})
+
+console.log(precosFiltrados, produtos)
